b9d643d092de300b9b0c111ffc6cd41af2c9904b
805534sh <128050379+805534sh@users.noreply.github.com>
2023-03-19 02:16:12 -0700

    Add files via upload

---
+++ assi2/Pat12.java
@@ -0,0 +1,21 @@
+class Pat12
+{
+public static void main(String args[])
+{
+    for(int i=1;i<=5;i++)
+	{
+	     for(int j=3;j>=i;j--)
+	      {
+	         System.out.print(" ");
+	        }
+	          for (int k=1;k<=5;k++)
+	            {
+	               System.out.println(i);
+	
+	                }
+                     for(int l=1;l<=5;l++)
+	                    {
+	                 System.out.print();
+	           }
+			   System.out.println();
+	}}}
---
+++ assi2/Pat2.java
@@ -0,0 +1,14 @@
+class Pat2
+{
+public static void main(String args[])
+{
+for(char i='A';i<='E';i++)
+{
+for(int j='A';j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.print('\n');
+}
+}
+}
